57185ddcb1c1b2b710a4e70f00fd9dc0a6dc530b
jarethholt <jareth.holt@gmail.com>
2024-02-29 21:17:16 +0100

    feat: Add MenuHelpers project
    Add the `MenuHelpers` project and namespace. This includes the `enum MenuOptions` and the class `MenuHelper` which displays the menu.

---
+++ Ovning2.MenuHelpers/MenuHelpers.cs
@@ -0,0 +1,34 @@
+﻿// Classes that support the main menu in Ovning2.
+
+using System.Text;
+
+namespace Ovning2.MenuHelpers;
+
+public enum MenuOptions : uint
+{
+    Quit
+}
+
+public class MenuHelper
+{
+    public static void DisplayMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to my console app!");
+        Console.WriteLine();
+        Console.WriteLine("Choose one of the options below.");
+
+        // Construct the options from the MenuOptions
+        string[] names = Enum.GetNames(typeof(MenuOptions));
+        var values = Enum.GetValues(typeof(MenuOptions));
+        int numOptions = names.Length;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < numOptions; i++)
+        {
+            stringBuilder.AppendLine($"{values.GetValue(i)}: {names[i]}");
+        }
+        Console.WriteLine(stringBuilder.ToString());
+        Console.WriteLine();
+    }
+}
