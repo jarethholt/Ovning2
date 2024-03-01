1037be8f9932a11ee76fa5a70aeab9953cbb4cd1
jarethholt <jareth.holt@gmail.com>
2024-03-01 13:51:32 +0100

    feat: Add SinglePriceDecider
    Add `SinglePriceDecider` class. This class has a single static method `DecidePrice` that returns the ticket price based on age, implementing all the logic of which category corresponds to which age range.

---
+++ Ovning2.Tickets/SinglePriceDecider.cs
@@ -0,0 +1,23 @@
+﻿// Class that helps decide the price of a single ticket.
+namespace Ovning2.Tickets;
+
+internal static class SinglePriceDecider
+{
+    public static uint DecidePrice(uint age)
+    {
+        CategoryName categoryName;
+        if (age < 20)
+        {
+            categoryName = CategoryName.Youth;
+        }
+        else if (age >= 64)
+        {
+            categoryName = CategoryName.Senior;
+        }
+        else
+        {
+            categoryName = CategoryName.Standard;
+        }
+        return TicketPrices.TicketCategories[categoryName];
+    }
+}
