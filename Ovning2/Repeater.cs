1552ac55bfecfcd790ea94d58626ff02e2ff1064
jarethholt <jareth.holt@gmail.com>
2024-03-01 09:49:28 +0100

    feat: Add Repeater class
    Add the `Repeater` class. It takes a single optional value - the number of times to repeat - and provides a single method `Repeat(string word)` that writes the word a given number of times to the console with spaces in between.

---
+++ Ovning2/Repeater.cs
@@ -0,0 +1,17 @@
+﻿// Class that repeats a given word 10 times.
+
+namespace Ovning2;
+
+internal class Repeater(uint numRepeats = 10)
+{
+    public uint NumRepeats { get; set; } = numRepeats;
+
+    public void Repeat(string word)
+    {
+        for (int i = 0; i < NumRepeats; i++)
+        {
+            Console.Write($"{word} ");
+        }
+        Console.WriteLine();
+    }
+}
