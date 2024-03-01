7b32e7d747f5861b77323f859724085fca0943e8
jarethholt <jareth.holt@gmail.com>
2024-03-01 13:37:29 +0100

    feat: Add TicketPrices file
    Add TicketPrices.cs in `Ovning2.Tickets` namespace. This provides the `enum CategoryName`, `struct TicketCategory`, and class `TicketPrices`. The main goal is to provide `TicketPrices.TicketCategories`, an array of (category, price) combinations.

---
+++ Ovning2.Tickets/TicketPrices.cs
@@ -0,0 +1,35 @@
+﻿// Helper class for holding ticket prices
+
+namespace Ovning2.Tickets;
+
+// Available ticket categories
+public enum CategoryName
+{
+    Youth,
+    Senior,
+    Standard
+}
+
+// Struct to hold description of a ticket category
+readonly struct TicketCategory
+{
+    public readonly CategoryName categoryName;
+    public readonly uint price;
+
+    public TicketCategory(CategoryName categoryName, uint price)
+    {
+        this.categoryName = categoryName;
+        this.price = price;
+    }
+}
+
+internal static class TicketPrices
+{
+    // Only contains a list of the ticket prices
+    public static readonly TicketCategory[] TicketCategories =
+    [
+        new(CategoryName.Youth, 80),
+        new(CategoryName.Senior, 90),
+        new(CategoryName.Standard, 120)
+    ];
+}
