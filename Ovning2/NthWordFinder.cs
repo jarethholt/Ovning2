a88707a5fa1292dd1e0d88b32ae4874e65d17608
jarethholt <jareth.holt@gmail.com>
2024-03-01 12:49:36 +0100

    feat: Add class NthWordFinder
    Add the class `NthWordFinder`. Behaves much like `Repeater` by providing the sole method `FindNthWord`.

---
+++ Ovning2/NthWordFinder.cs
@@ -0,0 +1,35 @@
+﻿// Class the finds the nth word in a sentence.
+
+namespace Ovning2;
+
+internal class NthWordFinder(uint wordNumber = 3)
+{
+    public uint WordNumber
+    {
+        get { return wordNumber; }
+        set
+        {
+            // Cannot accept 0 as a wordNumber
+            // Supposed to be the number, not the index
+            if (value == 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    paramName: nameof(value),
+                    message: "Cannot extract word number 0 from a sentence; use a value > 0");
+            }
+            wordNumber = value;
+        }
+    }
+
+    public string FindNthWord(string sentence)
+    {
+        string[] words = sentence.Split(' ', options: StringSplitOptions.RemoveEmptyEntries);
+        if (words.Length < WordNumber)
+        {
+            throw new ArgumentException(
+                message: $"Cannot find word number {WordNumber} in a sentence with {words.Length} words",
+                paramName: nameof(sentence));
+        }
+        return words[WordNumber - 1];  // Convert number to index
+    }
+}
