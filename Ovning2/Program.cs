744a8f348d84dc9a7ddabb9a1b89ae0207c6f6f0
jarethholt <jareth.holt@gmail.com>
2024-03-03 21:38:13 +0100

    Refactor: Introduce function to handle looping in Main
    For all of my sub-programs I like to allow the user to re-run the program without going back to the main menu. I added the `KeepLooping` function to abstract away that feature, so that each sub-program only has to run "itself" once.

--- Ovning2/Program.cs
+++ Ovning2/Program.cs
@@ -11,118 +11,103 @@ internal class Program
     static void Main(string[] args)
     {
         // Run the menu interface
+        bool again = true;
         do
         {
             MenuHelper.DisplayMenu();
             MenuOption option = Utilities.AskForOption("Your choice: ");
 
-            switch (option)
+            // Each action can be looped except for Quit
+            Action? action = null;
+            string againPrompt = "";
+
+            switch(option)
             {
                 case MenuOption.Quit:
-                    Environment.Exit(0);
+                    again = false;
                     break;
                 case MenuOption.SingleTicket:
-                    GetSingleTicketPrice();
+                    action = GetSingleTicketPrice;
+                    againPrompt = "Would you like to find another ticket price (y/n)? ";
                     break;
                 case MenuOption.RepeatWord:
-                    RepeatWord();
+                    action = RepeatWord;
+                    againPrompt = "Would you like to repeat another word (y/n)? ";
                     break;
                 case MenuOption.FindWord:
-                    FindWord();
+                    action = FindWord;
+                    againPrompt = "Would you like to do another sentence (y/n)? ";
                     break;
             }
 
-        } while (true);
+            if (again)
+            {
+                KeepLooping(action!, againPrompt);
+            }
+        } while (again);
     }
 
-    // Methods to run individual menu options
-    static void GetSingleTicketPrice()
+    // Helper function: Loop an action until asked to stop
+    static void KeepLooping(Action action, string againPrompt)
     {
+        bool again;
         do
         {
-            Console.Clear();
-            Console.WriteLine("Welcome to the ticket price info subprogram!");
-            Console.WriteLine("We can tell you the price of a single ticket based on your age.");
-            Console.WriteLine();
-            uint age = Utilities.AskForUInt("Please enter your age: ");
-
-            uint price = TicketPrices.DecidePrice(age);
+            action();
 
             Console.WriteLine();
-            Console.WriteLine($"The price of a ticket for your age {age} is {price}kr.");
-
-            Console.WriteLine();
-            bool again = Utilities.AskForYesNo("Would you like to find another ticket price (y/n)? ");
-            if (!again)
-            {
-                break;
-            }
-        } while (true);
+            again = Utilities.AskForYesNo(againPrompt);
+        } while (again);
 
         Console.WriteLine();
         Console.Write("Press enter to go back to the main menu.");
         _ = Console.ReadLine();
+    }
+
+    // Methods describing individual menu options
+    static void GetSingleTicketPrice()
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to the ticket price info subprogram!");
+        Console.WriteLine("We can tell you the price of a single ticket based on your age.");
+        Console.WriteLine();
+        uint age = Utilities.AskForUInt("Please enter your age: ");
+
+        uint price = TicketPrices.DecidePrice(age);
+
+        Console.WriteLine();
+        Console.WriteLine($"The price of a ticket for your age {age} is {price}kr.");
     }
 
     static void RepeatWord()
     {
-        do
-        {
-            Console.Clear();
-            Console.WriteLine("In this subprogram, you provide a word.");
-            Console.WriteLine("That word will then be repeated 10 times in the console.");
-            Console.WriteLine();
-            string word = Utilities.AskForString("What word should be repeated? ");
-            Console.WriteLine();
-
-            repeater.Repeat(word);
-
-            Console.WriteLine();
-            bool again = Utilities.AskForYesNo("Would you like to repeat another word (y/n)? ");
-            if (!again)
-            {
-                break;
-            }
-        } while (true);
-
-        // End of sub-program; wait for user response
+        Console.Clear();
+        Console.WriteLine("In this subprogram, you provide a word.");
+        Console.WriteLine("That word will then be repeated 10 times in the console.");
         Console.WriteLine();
-        Console.Write("Press enter to go back to the main menu.");
-        _ = Console.ReadLine();
+        string word = Utilities.AskForString("What word should be repeated? ");
+        Console.WriteLine();
+
+        repeater.Repeat(word);
     }
 
     static void FindWord()
     {
-        do
-        {
-            Console.Clear();
-            Console.WriteLine("In this subprogram, you provide a sentence.");
-            Console.WriteLine("We will then find the 3rd word in the sentence.");
-            Console.WriteLine();
-            string sentence = Utilities.AskForString("What is your sentence?\n");
-            Console.WriteLine();
-
-            try
-            {
-                string word = wordFinder.FindWord(sentence);
-                Console.WriteLine($"The 3rd word in the sentence is: '{word}'");
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            Console.WriteLine();
-            bool again = Utilities.AskForYesNo("Would you like to do another sentence (y/n)? ");
-            if (!again)
-            {
-                break;
-            }
-        } while (true);
-
-        // End of sub-program; wait for user input
+        Console.Clear();
+        Console.WriteLine("In this subprogram, you provide a sentence.");
+        Console.WriteLine("We will then find the 3rd word in the sentence.");
         Console.WriteLine();
-        Console.Write("Press enter to go back to the main menu.");
-        _ = Console.ReadLine();
+        string sentence = Utilities.AskForString("What is your sentence?\n");
+        Console.WriteLine();
+
+        try
+        {
+            string word = wordFinder.FindWord(sentence);
+            Console.WriteLine($"The 3rd word in the sentence is: '{word}'");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
