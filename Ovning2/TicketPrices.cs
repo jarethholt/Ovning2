1e0a0f76c69332cb4098b72474b68e1623e366d2
jarethholt <jareth.holt@gmail.com>
2024-03-04 13:42:08 +0100

    refactor: Place all files under Ovning2 folder

---
+++ Ovning2/TicketPrices.cs
@@ -0,0 +1,91 @@
+﻿// Helper class for holding ticket prices and categories
+
+namespace Ovning2.Tickets;
+
+// Available ticket categories
+// enum isn't necessary but I wanted to try it out
+public enum CategoryName
+{
+    Youth,
+    Senior,
+    Standard
+}
+
+public static class TicketPrices
+{
+    // Only contains a list of the ticket prices
+    public static readonly Dictionary<CategoryName, uint> TicketCategories = new()
+    {
+        { CategoryName.Youth   ,  80 },
+        { CategoryName.Senior  ,  90 },
+        { CategoryName.Standard, 120 }
+    };
+
+    public static uint DecidePrice(uint age)
+    {
+        CategoryName categoryName;
+        if (age < 20)
+            categoryName = CategoryName.Youth;
+        else if (age > 64)
+            categoryName = CategoryName.Senior;
+        else
+            categoryName = CategoryName.Standard;
+        return TicketCategories[categoryName];
+    }
+
+    public static uint DecideTotalPrice(uint[] ages)
+    {
+        // Decide the total price from an array of ages
+        // Uses a straightforward calculation method
+        uint total = 0;
+        foreach (uint age in ages)
+        {
+            total += DecidePrice(age);
+        }
+        return total;
+    }
+}
+
+
+/* Similarly, this implementation was more for me to practice
+ * defining and passing delegates and using some LINQ functionality
+ */
+
+// Type of function that determines whether someone fits an age category
+public delegate bool AgeCheck(uint age);
+
+// Type of tuple that combines an AgeCheck with the associated price
+public class CheckAndPrice(AgeCheck checker, uint price)
+{
+    public AgeCheck Checker { get; init; } = checker;
+    public uint Price { get; init; } = price;
+}
+
+public static class TicketPrices2
+{
+    public static readonly Dictionary<CategoryName, CheckAndPrice> TicketCategories = new()
+    {
+        { CategoryName.Youth   , new((uint age) => age < 20,  80) },
+        { CategoryName.Senior  , new((uint age) => age > 64,  90) },
+        { CategoryName.Standard, new((uint age) => true    , 120) }
+    };
+
+    public static uint DecidePrice(uint age)
+    {
+        // Decide the price using LINQ.First (for practicing with LINQ)
+        KeyValuePair<CategoryName, CheckAndPrice> kvp = TicketCategories
+            .First(kvp => kvp.Value.Checker(age));
+        return kvp.Value.Price;
+    }
+
+    private static int DecidePrice(int age)
+    {
+        return (int)DecidePrice((uint)age);
+    }
+
+    public static uint DecideTotalPrice(IEnumerable<uint> ages) =>
+        // Decide the price using LINQ functions
+        // LINQ.Sum is not provided for uint, only signed integers
+        (uint)ages.Select(Convert.ToInt32).Sum(DecidePrice);
+}
+
