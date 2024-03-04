02155da5a71b501c6d9b2928b87c002c0c4c3fb4
jarethholt <jareth.holt@gmail.com>
2024-03-04 11:40:49 +0100

    feat: Add descriptions to the main menu
    Add short descriptions of each subprogram to the main menu. The descriptions are held in `MenuHelper.Descriptions`.

--- Ovning2.MenuHelpers/MenuHelpers.cs
+++ Ovning2.MenuHelpers/MenuHelpers.cs
@@ -6,15 +6,28 @@ namespace Ovning2.MenuHelpers;
 
 public enum MenuOption : uint
 {
-    Quit,
+    Quit,          // Quit the program
     SingleTicket,  // Single ticket price
-    GroupTicket,  // Group ticket price
-    RepeatWord,  // Repeat a word
-    FindWord  // Find 3rd word in a sentence
+    GroupTicket,   // Group ticket price
+    RepeatWord,    // Repeat a word
+    FindWord       // Find 3rd word in a sentence
 }
 
-public class MenuHelper
+public static class MenuHelper
 {
+    public static readonly Dictionary<MenuOption, string> Descriptions = new()
+    {
+        { MenuOption.Quit,         "Quit this program" },
+        { MenuOption.SingleTicket, "Find the price of a single movie ticket" },
+        { MenuOption.GroupTicket,  "Calculate the total price of tickets for a group" },
+        { MenuOption.RepeatWord,   "Repeat a given word 10 times" },
+        { MenuOption.FindWord,     "Find the third word in a sentence" }
+    };
+
+    private static readonly int MaxOptionLength =
+        Enum.GetValues(typeof(MenuOption)).Cast<MenuOption>()
+        .Select(option => option.ToString().Length).Max();
+
     public static void DisplayMenu()
     {
         Console.Clear();
@@ -24,10 +37,14 @@ public class MenuHelper
 
         // Construct the options from the MenuOptions
         StringBuilder stringBuilder = new();
+        string format = string.Format(
+            "{0}0{1} : {0}1,-{2}{1} : {0}2{1}",
+            "{", "}", MaxOptionLength);
 
         foreach (MenuOption option in Enum.GetValues(typeof(MenuOption)))
         {
-            stringBuilder.AppendLine($"{(uint)option}: {option}");
+            stringBuilder.AppendLine(string.Format(
+                format, (uint)option, option, Descriptions[option]));
         }
         Console.WriteLine(stringBuilder.ToString());
         Console.WriteLine();
